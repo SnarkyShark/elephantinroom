e220a295fa27b86a505a7f25878ab620d581517f
Will Johannesen <willjohannesen@gmail.com>
2019-01-01 13:42:44 -0500

    Replace Elephant
    Full gameplay loop finished--UI is left

--- ElelephantJam/Assets/Scripts/TriggerElephant.cs
+++ ElelephantJam/Assets/Scripts/TriggerElephant.cs
@@ -6,10 +6,12 @@ public class TriggerElephant : MonoBehaviour
 {
     public GameObject objectToActivate;
     public GameObject objectToDeactivate;
+    public GameObject selfObjectDeactivate;
 
     void OnTriggerEnter(Collider other) {
       if(other.CompareTag("Player")) {
         objectToActivate.SetActive(true);
+        selfObjectDeactivate.SetActive(false);
         objectToDeactivate.SetActive(false);
       }
     }
